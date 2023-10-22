c73c87d6b34e12f0ec9094e7ed86d63bbc608ae7
Daiwei Lu <daiweilu123@gmail.com>
2023-10-22 13:18:15 -0700

    Simplify example

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
 import serverless from "serverless-http";
 import express from "express";
-import {
-  APIGatewayProxyResult,
-  APIGatewayProxyEvent,
-  Context,
-} from "aws-lambda";
 
 const app = express();
 
@@ -12,9 +7,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const serverlessHandler = serverless(app);
-
-export async function handler(event: APIGatewayProxyEvent, context: Context) {
-  const result = await serverlessHandler(event, context);
-  return result as APIGatewayProxyResult;
-}
+export const handler = serverless(app);
